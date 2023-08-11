13533b2a36948897b67131b682d6bef7caee9d70
dingzeyuan <zdin0570@uni.sydney.edu.au>
2023-08-11 11:22:09 +1000

    update the download location of dataset

--- data_loaders.py
+++ data_loaders.py
@@ -8,9 +8,9 @@ def cifar10():
         transform_test = transforms.Compose([
             transforms.ToTensor(),
         ])
-        train_dataset = CIFAR10(root='E:\datasets',
+        train_dataset = CIFAR10(root='\datasets',
                                 train=True, download=True, transform=transform_train)
-        val_dataset = CIFAR10(root='E:\datasets',
+        val_dataset = CIFAR10(root='\datasets',
                               train=False, download=True, transform=transform_test)
         norm = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         return train_dataset, val_dataset, norm
